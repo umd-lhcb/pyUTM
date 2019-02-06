590817cddee94265aa5d4707a380a487b08ecf7a
Zishuo Yang <yangzs@umd.edu>
2019-02-06 15:05:04 -0500

    Add table for swapping JD# from Proto to Mirror

--- pyUTM/common.py
+++ pyUTM/common.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
 # License: BSD 2-clause
-# Last Change: Wed Feb 06, 2019 at 10:41 AM -0500
+# Last Change: Thu Dec 20, 2018 at 12:16 PM -0500
 
 from collections import defaultdict
 
@@ -9,6 +9,7 @@ from collections import defaultdict
 # Constants #
 #############
 
+# Table for swapping JD# from Proto(right-hand side) to True(left-hand side)
 jd_swapping_true = {
     'JD0': 'JD0',
     'JD1': 'JD4',
@@ -24,21 +25,21 @@ jd_swapping_true = {
     'JD11': 'JD11'
 }
 
-jp_flex_type_proto = {
-    'JP0': 'X-0-M',
-    'JP1': 'X-0-S',
-    'JP2': 'S-0-S',
-    'JP3': 'S-0-M',
-    'JP4': 'X-1-M',
-    'JP5': 'X-1-S',
-    'JP6': 'S-1-S',
-    'JP7': 'S-1-M',
-    'JP8': 'X-2-M',
-    'JP9': 'X-2-S',
-    'JP10': 'S-2-S',
-    'JP11': 'S-2-M',
+# Table for swapping JD# from Proto(right-hand side) to Mirror(left-hand side)
+jd_swapping_mirror = {
+    'JD0': 'JD1',
+    'JD1': 'JD5',
+    'JD2': 'JD3',
+    'JD3': 'JD2',
+    'JD4': 'JD0',
+    'JD5': 'JD4',
+    'JD6': 'JD7',
+    'JD7': 'JD9',
+    'JD8': 'JD6',
+    'JD9': 'JD8',
+    'JD10': 'JD11',
+    'JD11': 'JD10'
 }
-
 
 #############################
 # For YAML/Excel conversion #
