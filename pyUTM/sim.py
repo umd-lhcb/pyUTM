62767d0b6e417bd44db0bc48ed6ff2c12099a1da
Yipeng Sun <syp@umd.edu>
2019-02-11 10:17:44 -0500

    Resurrect the old recursion function.

--- pyUTM/sim.py
+++ pyUTM/sim.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 #
 # License: BSD 2-clause
-# Last Change: Fri Feb 08, 2019 at 12:46 PM -0500
+# Last Change: Mon Feb 11, 2019 at 10:17 AM -0500
 
 import re
+
+from collections import defaultdict
 
 
 ##########################
@@ -13,6 +15,9 @@ import re
 class CurrentFlow(object):
     def __init__(self, passable=[r'^R\d+', r'^C\d+', r'^NT\d+']):
         self.passable = passable
+
+    def do(self):
+        pass
 
     def strip(self, d):
         result = {}
@@ -26,10 +31,52 @@ class CurrentFlow(object):
 
         return result
 
+    @classmethod
+    def find_all_flows(cls,
+                       netname,
+                       net_to_comp, comp_to_net,
+                       connected_nets=None, hopped_components=None,
+                       num_of_recursion=0, max_num_of_recursion=900):
+        if num_of_recursion == 0:
+            connected_nets = []
+            hopped_components = []
+
+        if num_of_recursion > max_num_of_recursion:
+            raise ValueError(
+                'Cannot exhaust hoppable components within {}. Giving up.'.format(
+                    max_num_of_recursion
+                ))
+
+        if netname not in connected_nets:
+            connected_nets.append(netname)
+
+        if hopped_components == net_to_comp[netname]:
+            return connected_nets
+
+        else:
+            unhopped_components = cls.diff(
+                comp_to_net(netname, hopped_components))
+            for component in unhopped_components:
+                unsurveyed_net = [i for i in comp_to_net[component]
+                                  if i != netname]
+                for net in unsurveyed_net:
+                    cls.find_all_flows(
+                        net,
+                        net_to_comp, comp_to_net,
+                        connected_nets, [component]+hopped_components,
+                        num_of_recursion+1)
+
+            return connected_nets
+
     @staticmethod
-    def flow(avail_comps, comp_to_net):
-        comp = avail_comps[0]
-        return (comp, comp_to_net[comp])
+    def swap_key_to_value(d):
+        converted = defaultdict(list)
+
+        for key, values in d.items():
+            for i in values:
+                converted[i].append(key)
+
+        return converted
 
     @staticmethod
     def diff(l1, l2):
